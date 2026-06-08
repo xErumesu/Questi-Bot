897a5d274ebee53fea1eb907faa8336e03b3568b
zuriminal <avanicole501@gmail.com>
2026-06-08 14:24:29 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "EMQ.",
+        name: "https://www.youtube.com/@EveryonesMorallyQuestionable",
         // Activity type number (0 = Playing).
         type: 3, 
       },
