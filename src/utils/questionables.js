7a2f8fa625cc4ab3383df5b139870d19ecb71067
zuriminal <avanicole501@gmail.com>
2026-06-10 02:23:38 -0400

    Update questionables.js

--- src/utils/questionables.js
+++ src/utils/questionables.js
@@ -4,67 +4,49 @@ export const questionables = [
     name: 'Leo Questionable',
     type: 'regular',
     rarity: 'Common',
-
     image: '',
-
     catchAnswer: 'questionable',
     catchTime: 60000,
   },
-
   {
-    id: 'Soba',
+    id: 'soba',
     name: 'Soba Questionable',
     type: 'regular',
     rarity: 'Uncommon',
-
     image: '',
-
     catchAnswer: 'questionable',
     catchTime: 60000,
   },
-
   {
     id: 'rylan',
     name: 'Rylan Questionable',
     type: 'regular',
     rarity: 'Rare',
-
     image: '',
-
     catchAnswer: 'questionable',
     catchTime: 60000,
   },
-
   {
     id: 'star',
     name: 'Star Questionable',
     type: 'regular',
     rarity: 'Epic',
-
     image: '',
-
     catchAnswer: 'questionable',
     catchTime: 60000,
   },
-
-  // CUSTOMS
-
   {
     id: 'captain',
     name: 'Captain Questionable',
     type: 'custom',
-
+    rarity: 'Custom',
     image: '',
-
     catchAnswer: 'captain questionable',
     catchTime: 15000,
-
     spawnTitle: '👑 Captain Questionable appeared!',
-    spawnText:
-      '👑 Captain Questionable has arrived.\n\nUse `/catch captain questionable` before he leaves!',
-
-    escapeText:
-      '💨 Captain Questionable escaped before anyone could recruit him.'
+    spawnText: '👑 Captain Questionable has arrived.\n\nUse `/catch captain questionable` before he leaves!',
+    escapeText: '💨 Captain Questionable escaped before anyone could recruit him.',
+    catchText: '⚓ {user} caught **Captain Questionable** before he escaped!'
   },
 ];
 
@@ -73,11 +55,6 @@ export function getQuestionableById(id) {
 }
 
 export function getRandomQuestionable() {
-  const regulars = questionables.filter(
-    q => q.type === 'regular'
-  );
-
-  return regulars[
-    Math.floor(Math.random() * regulars.length)
-  ];
+  const regulars = questionables.filter(q => q.type === 'regular');
+  return regulars[Math.floor(Math.random() * regulars.length)];
 }
